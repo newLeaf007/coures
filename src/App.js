551c626c43e3677d2649bb049748a6c73d74f795
newLeafOO7 <raushankumarcse137@gmail.com>
2022-08-16 16:17:16 +0530

    api access and show in ui with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <h1>This is React App</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        provident reiciendis molestiae architecto doloremque, explicabo ullam
+        pariatur quis sed eos? Laudantium voluptate magnam, labore pariatur rem
+        officiis velit ducimus perspiciatis.
+      </p>
     </div>
   );
 }
